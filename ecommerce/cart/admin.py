6c7c0698c189367b8429a2c1bf05ab4c9cba9114
stejimaria <stejimaria00@gmail.com>
2024-10-31 22:55:57 +0530

    added1

--- ecommerce/cart/admin.py
+++ ecommerce/cart/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from cart.models import Cart,Order_details,Payment
 
+from django.http import HttpResponse
 # Register your models here.
 admin.site.register(Cart)
 admin.site.register(Order_details)
